b12c62d34ae7b2e66d304e16472070bf5c458df3
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2024-03-05 13:04:18 +0000

    spacy file creation

---
+++ myenv/src/spacyTest.py
@@ -0,0 +1,20 @@
+import bs4 as bs
+import urllib.request
+import spacy
+
+pln = spacy.load('pt_core_news_sm')
+
+document = pln('Estou aprendendo processamento de linguagem natural, curso em Braganca')
+
+for token in document:
+    print(token.text, token.pos_)
+
+# Changing aprendendo to the verb aprender
+for token in document:
+    print(token.text, token.lemma_)
+
+doc = pln('encontrei encontraram encontrarao encontrariam cursando curso cursei')
+
+# Changing aprendendo to the verb aprender
+for token in doc:
+    print(token.text, token.lemma_)
