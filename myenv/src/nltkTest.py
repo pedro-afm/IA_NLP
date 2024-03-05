7f7bec7003b461f211b147596b5a0bfc07f8af01
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2024-03-05 13:22:23 +0000

    stemizacao

--- myenv/src/nltkTest.py
+++ myenv/src/nltkTest.py
@@ -1,3 +1,16 @@
 import nltk
+import bs4 as bs
+import urllib.request
+import spacy
 
 nltk.download('rslp')
+
+pln = spacy.load('pt_core_news_sm')
+
+document = pln('Estou aprendendo processamento de linguagem natural, curso em Braganca')
+
+stemmer = nltk.stem.RSLPStemmer()
+stemmer.stem('aprender')
+
+for token in document:
+    print(token.text, token.lemma_, stemmer.stem(token.text))
